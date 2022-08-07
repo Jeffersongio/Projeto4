e5452e15b7e3761a05174d2b789bda76d67c6db2
Jeffersongio <110689946+Jeffersongio@users.noreply.github.com>
2022-08-07 13:13:05 -0300

    Atualização

--- project/settings.py
+++ project/settings.py
@@ -118,6 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
